20a0545620424cb93af5f993c79cf09f75d3b086
Kriro-Lab <krirokriro@gmail.com>
2026-03-14 19:43:30 +1000

    Delete app/app directory

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Tai Chi Guide</h1>
-      <p>Welcome to your Tai Chi training app.</p>
-    </main>
-  )
-}
